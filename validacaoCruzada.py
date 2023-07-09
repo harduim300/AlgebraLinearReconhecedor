da78c9e11e9d23e0df75e4efee56bc1c3a1b1b97
Matheus Harduim <66342070+harduim300@users.noreply.github.com>
2023-07-09 18:47:46 -0300

    Update validacaoCruzada.py

--- validacaoCruzada.py
+++ validacaoCruzada.py
@@ -22,14 +22,14 @@ def mainExecucao(kIteracoes, usarPCA, banco_imagens, tipo_Mapeamento, n_indices)
     return float(percentual)/kIteracoes
 
 
-# ResultFB = "Força Bruta: " + str(mainExecucao(5, False, 'recdev-master/very-easy/', 'VE', 2)) + "%"
-# ResultPCA = "PCA: " + str(mainExecucao(10, True, 'recdev-master/very-easy/', 'VE', 2)) + "%"
-# print("-----------------------------------------")
-# print("TAXAS DE ACERTO PARA: ")
-# print("VERY-EASY")
-# print(ResultFB)
-# print(ResultPCA)
-# print("-----------------------------------------")
+ResultFB = "Força Bruta: " + str(mainExecucao(5, False, 'recdev-master/very-easy/', 'VE', 2)) + "%"
+ResultPCA = "PCA: " + str(mainExecucao(10, True, 'recdev-master/very-easy/', 'VE', 2)) + "%"
+print("-----------------------------------------")
+print("TAXAS DE ACERTO PARA: ")
+print("VERY-EASY")
+print(ResultFB)
+print(ResultPCA)
+print("-----------------------------------------")
 # ResultFB = "Força Bruta: " + str(mainExecucao(5, False, 'recdev-master/easy/', 'EAS', 8)) + "%"
 # ResultPCA = "PCA: " + str(mainExecucao(10, True, 'recdev-master/easy/', 'EAS', 8)) + "%"
 # print("-----------------------------------------")
@@ -54,51 +54,51 @@ def mainExecucao(kIteracoes, usarPCA, banco_imagens, tipo_Mapeamento, n_indices)
 # print(ResultFB)
 # print(ResultPCA)
 # print("-----------------------------------------")
-ResultFB = "Força Bruta: " + str(mainExecucao(3, False, 'recdev-master/extras/facebookfaces/', 'FB', 9)) + "%"
-ResultPCA = "PCA: " + str(mainExecucao(3, True, 'recdev-master/extras/facebookfaces/', 'FB', 9)) + "%"
-print("-----------------------------------------")
-print("TAXAS DE ACERTO PARA: ")
-print("FACEBOOK 1 TESTE INNER-OUTER")
-print(ResultFB)
-print(ResultPCA)
-print("-----------------------------------------")
-ResultFB = "Força Bruta: " + str(mainExecucao(3, False, 'recdev-master/extras/facebookfaces-2/', 'FB2', 10)) + "%"
-ResultPCA = "PCA: " + str(mainExecucao(3, True, 'recdev-master/extras/facebookfaces-2/', 'FB2', 10)) + "%"
-print("-----------------------------------------")
-print("TAXAS DE ACERTO PARA: ")
-print("FACEBOOK 2 TESTE INNER-OUTER")
-print(ResultFB)
-print(ResultPCA)
-print("-----------------------------------------")
-ResultFB = "Força Bruta: " + str(mainExecucao(1, False, 'recdev-master/extras/facebookfaces/crop-inner/', 'PER', 9)) + "%"
-ResultPCA = "PCA: " + str(mainExecucao(1, True, 'recdev-master/extras/facebookfaces/crop-inner/', 'PER', 9)) + "%"
-print("-----------------------------------------")
-print("TAXAS DE ACERTO PARA: ")
-print("FACEBOOK 1 TESTE INNER")
-print(ResultFB)
-print(ResultPCA)
-print("-----------------------------------------")
-ResultFB = "Força Bruta: " + str(mainExecucao(1, False, 'recdev-master/extras/facebookfaces-2/crop-inner/', 'PER2', 10)) + "%"
-ResultPCA = "PCA: " + str(mainExecucao(1, True, 'recdev-master/extras/facebookfaces-2/crop-inner/', 'PER2', 10)) + "%"
-print("-----------------------------------------")
-print("TAXAS DE ACERTO PARA: ")
-print("FACEBOOK 2 TESTE INNER")
-print(ResultFB)
-print(ResultPCA)
-print("-----------------------------------------")
-ResultFB = "Força Bruta: " + str(mainExecucao(1, False, 'recdev-master/extras/facebookfaces/crop-outer/', 'PER', 9)) + "%"
-ResultPCA = "PCA: " + str(mainExecucao(1, True, 'recdev-master/extras/facebookfaces/crop-outer/', 'PER', 9)) + "%"
-print("-----------------------------------------")
-print("TAXAS DE ACERTO PARA: ")
-print("FACEBOOK 1 TESTE OUTER")
-print(ResultFB)
-print(ResultPCA)
-print("-----------------------------------------")
-ResultFB = "Força Bruta: " + str(mainExecucao(1, False, 'recdev-master/extras/facebookfaces-2/crop-outer/', 'PER2', 10)) + "%"
-ResultPCA = "PCA: " + str(mainExecucao(1, True, 'recdev-master/extras/facebookfaces-2/crop-outer/', 'PER2', 10)) + "%"
-print("-----------------------------------------")
-print("TAXAS DE ACERTO PARA: ")
-print("FACEBOOK 2 TESTE OUTER")
-print(ResultFB)
-print(ResultPCA)
-print("-----------------------------------------")
+#ResultFB = "Força Bruta: " + str(mainExecucao(3, False, 'recdev-master/extras/facebookfaces/', 'FB', 9)) + "%"
+# ResultPCA = "PCA: " + str(mainExecucao(3, True, 'recdev-master/extras/facebookfaces/', 'FB', 9)) + "%"
+# print("-----------------------------------------")
+# print("TAXAS DE ACERTO PARA: ")
+# print("FACEBOOK 1 TESTE INNER-OUTER")
+# print(ResultFB)
+# print(ResultPCA)
+# print("-----------------------------------------")
+# ResultFB = "Força Bruta: " + str(mainExecucao(3, False, 'recdev-master/extras/facebookfaces-2/', 'FB2', 10)) + "%"
+# ResultPCA = "PCA: " + str(mainExecucao(3, True, 'recdev-master/extras/facebookfaces-2/', 'FB2', 10)) + "%"
+# print("-----------------------------------------")
+# print("TAXAS DE ACERTO PARA: ")
+# print("FACEBOOK 2 TESTE INNER-OUTER")
+# print(ResultFB)
+# print(ResultPCA)
+# print("-----------------------------------------")
+# ResultFB = "Força Bruta: " + str(mainExecucao(1, False, 'recdev-master/extras/facebookfaces/crop-inner/', 'PER', 9)) + "%"
+# ResultPCA = "PCA: " + str(mainExecucao(1, True, 'recdev-master/extras/facebookfaces/crop-inner/', 'PER', 9)) + "%"
+# print("-----------------------------------------")
+# print("TAXAS DE ACERTO PARA: ")
+# print("FACEBOOK 1 TESTE INNER")
+# print(ResultFB)
+# print(ResultPCA)
+# print("-----------------------------------------")
+# ResultFB = "Força Bruta: " + str(mainExecucao(1, False, 'recdev-master/extras/facebookfaces-2/crop-inner/', 'PER2', 10)) + "%"
+# ResultPCA = "PCA: " + str(mainExecucao(1, True, 'recdev-master/extras/facebookfaces-2/crop-inner/', 'PER2', 10)) + "%"
+# print("-----------------------------------------")
+# print("TAXAS DE ACERTO PARA: ")
+# print("FACEBOOK 2 TESTE INNER")
+# print(ResultFB)
+# print(ResultPCA)
+# print("-----------------------------------------")
+# ResultFB = "Força Bruta: " + str(mainExecucao(1, False, 'recdev-master/extras/facebookfaces/crop-outer/', 'PER', 9)) + "%"
+# ResultPCA = "PCA: " + str(mainExecucao(1, True, 'recdev-master/extras/facebookfaces/crop-outer/', 'PER', 9)) + "%"
+# print("-----------------------------------------")
+# print("TAXAS DE ACERTO PARA: ")
+# print("FACEBOOK 1 TESTE OUTER")
+# print(ResultFB)
+# print(ResultPCA)
+# print("-----------------------------------------")
+# ResultFB = "Força Bruta: " + str(mainExecucao(1, False, 'recdev-master/extras/facebookfaces-2/crop-outer/', 'PER2', 10)) + "%"
+# ResultPCA = "PCA: " + str(mainExecucao(1, True, 'recdev-master/extras/facebookfaces-2/crop-outer/', 'PER2', 10)) + "%"
+# print("-----------------------------------------")
+# print("TAXAS DE ACERTO PARA: ")
+# print("FACEBOOK 2 TESTE OUTER")
+# print(ResultFB)
+# print(ResultPCA)
+# print("-----------------------------------------")
