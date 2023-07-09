8bb94c886a3d0a29fea4d51abd30d7c5facc020b
harduim300 <harduim300@gmail.com>
2023-07-09 14:31:12 -0300

    Adiciona arquivos novos

--- reconhecedor.py
+++ reconhecedor.py
@@ -2,6 +2,7 @@ from PIL import Image
 import random
 import os
 import numpy as np
+import cv2
 import matplotlib.image as mpimg
 import matplotlib.pyplot as plt
 import sklearn.decomposition as decomp
@@ -29,7 +30,8 @@ def treinamentoDeBasesUnicas(usarPCA, banco_imagens, tipo_Mapeamento, n_indices,
     vetorImagens = []
     imagensLabel = []
     imagensDeTreino = None
-    if tipo_Mapeamento == 'FB' or tipo_Mapeamento == 'FB2':
+    mapeamentoFB = tipo_Mapeamento == 'FB'or tipo_Mapeamento == 'FB2'
+    if mapeamentoFB:
         caminho = banco_imagens + "crop-inner/"
     else:
         caminho = banco_imagens
@@ -50,15 +52,27 @@ def treinamentoDeBasesUnicas(usarPCA, banco_imagens, tipo_Mapeamento, n_indices,
                     imagensDeTeste.append(arquivo_img_escolh)
                     mapeamento[ids].append(arquivo_img_escolh)
                     if usarPCA == True:
-                        vetorImagens.append(leituraImagem(caminho,arquivo_img_escolh))
-                        imagensLabel.append(arquivo_img_escolh.split("-")[0])
+                        if mapeamentoFB:
+                            imgCinza = cv2.imread(os.path.join(caminho, arquivo_img_escolh), cv2.IMREAD_GRAYSCALE)
+                            matrizImagem = np.array(imgCinza).flatten()
+                            vetorImagens.append(matrizImagem)
+                            imagensLabel.append(arquivo_img_escolh.split("-")[0])
+                        else:    
+                            vetorImagens.append(leituraImagem(caminho,arquivo_img_escolh))
+                            imagensLabel.append(arquivo_img_escolh.split("-")[0])
                 elif i == ids_escolh and insersaoTeste == "N":
                     imagensDeTeste.append(arquivo_img_escolh)
                 else:
                     mapeamento[ids].append(arquivo_img_escolh)
                     if usarPCA == True:
-                        vetorImagens.append(leituraImagem(caminho,arquivo_img_escolh))
-                        imagensLabel.append(arquivo_img_escolh.split("-")[0])
+                        if mapeamentoFB:
+                            imgCinza = cv2.imread(os.path.join(caminho, arquivo_img_escolh), cv2.IMREAD_GRAYSCALE)
+                            matrizImagem = np.array(imgCinza).flatten()
+                            vetorImagens.append(matrizImagem)
+                            imagensLabel.append(arquivo_img_escolh.split("-")[0])
+                        else:    
+                            vetorImagens.append(leituraImagem(caminho,arquivo_img_escolh))
+                            imagensLabel.append(arquivo_img_escolh.split("-")[0])
 
                 
     imagensDeTreino = mapeamento
@@ -70,7 +84,8 @@ def treinamentoDeBasesTamannhoVariado(usarPCA, banco_imagens, tipo_Mapeamento, n
     vetorImagens = []
     imagensLabel = []
     imagensDeTreino = None
-    if tipo_Mapeamento == 'FB':
+    mapeamentoFB = tipo_Mapeamento == 'FB'or tipo_Mapeamento == 'FB2'
+    if mapeamentoFB:
         caminho = banco_imagens + "crop-inner/"
     else:
         caminho = banco_imagens
@@ -93,15 +108,27 @@ def treinamentoDeBasesTamannhoVariado(usarPCA, banco_imagens, tipo_Mapeamento, n
                     imagensDeTeste.append(arquivo_img_escolh)
                     mapeamento[ids].append(arquivo_img_escolh)
                     if usarPCA == True:
-                        vetorImagens.append(leituraImagem(caminho,arquivo_img_escolh))
-                        imagensLabel.append(arquivo_img_escolh.split("-")[0])
+                        if mapeamentoFB:
+                            imgCinza = cv2.imread(os.path.join(caminho, arquivo_img_escolh), cv2.IMREAD_GRAYSCALE)
+                            matrizImagem = np.array(imgCinza).flatten()
+                            vetorImagens.append(matrizImagem)
+                            imagensLabel.append(arquivo_img_escolh.split("-")[0])
+                        else:    
+                            vetorImagens.append(leituraImagem(caminho,arquivo_img_escolh))
+                            imagensLabel.append(arquivo_img_escolh.split("-")[0])
                 elif i == ids_escolh and insersaoTeste == "N":
                     imagensDeTeste.append(arquivo_img_escolh)
                 else:
                     mapeamento[ids].append(arquivo_img_escolh)
                     if usarPCA == True:
-                        vetorImagens.append(leituraImagem(caminho,arquivo_img_escolh))
-                        imagensLabel.append(arquivo_img_escolh.split("-")[0])
+                        if mapeamentoFB:
+                            imgCinza = cv2.imread(os.path.join(caminho, arquivo_img_escolh), cv2.IMREAD_GRAYSCALE)
+                            matrizImagem = np.array(imgCinza).flatten()
+                            vetorImagens.append(matrizImagem)
+                            imagensLabel.append(arquivo_img_escolh.split("-")[0])
+                        else:    
+                            vetorImagens.append(leituraImagem(caminho,arquivo_img_escolh))
+                            imagensLabel.append(arquivo_img_escolh.split("-")[0])
 
                 
     imagensDeTreino = mapeamento
@@ -137,6 +164,24 @@ def leituraImagem(banco_imagens ,nome_arquivo):
     imgCinza = Image.open(banco_imagens + nome_arquivo).convert('L')
     matrizImagem = np.array(imgCinza)
     return matrizImagem.flatten()
+
+def leituraImagemFB(faceCascade, banco_imagens ,nome_arquivo):
+    imgCinza = cv2.imread(os.path.join(banco_imagens, nome_arquivo), cv2.IMREAD_GRAYSCALE)
+    detectarFaces = faceCascade.detectMultiScale(imgCinza,scaleFactor=1.3,minNeighbors=3, minSize=(30, 30))
+    
+    if len(detectarFaces) == 0:
+        # Se nenhum rosto for encontrado, continue para a próxima imagem
+        return [], leituraImagem(banco_imagens ,nome_arquivo)
+    
+    # Pegando localização do rosto detectado, como só possui um rosto por foto!
+    (x, y, w, h) = detectarFaces[0]
+
+    # Isolando o rosto da imagem
+    faceExtraida = imgCinza[y:y+h, x:x+w]
+
+    # Redimensionando a imagem do rosto para um tamanho padrão
+    faceExtraidaRedimencionando = cv2.resize(faceExtraida, (100, 100))
+    return np.array(faceExtraidaRedimencionando).flatten(), leituraImagem(banco_imagens ,nome_arquivo)
 
 
 def classificador(nome_arquivo):
@@ -174,18 +219,27 @@ def pcaClassificacao(img, imagensLabel, vetorImagens):
 def reconhecedor(resultado, usarPCA,banco_imagens, imagens_teste, imagens_treino, imagensLabel, vetorImagens, tipo_Mapeamento):
     errado = 0
     certo = 0
+
+    if tipo_Mapeamento == 'FB' or tipo_Mapeamento == 'FB2':
+        # Carregando o classificador pré-treinado para detecção de rostos
+        faceCascade = cv2.CascadeClassifier(cv2.data.haarcascades + '/haarcascade_frontalface_default.xml')
     for fn in imagens_teste:
         caminho = ""
         if tipo_Mapeamento == 'FB' or tipo_Mapeamento == 'FB2':
             caminho = banco_imagens + "crop-outer/"
-            img = leituraImagem(caminho, fn)
+            img, imgNaoEncontrado = leituraImagemFB(faceCascade, caminho, fn)
         else:
             caminho = banco_imagens
             img = leituraImagem(caminho, fn)
-        if usarPCA == True:
-            aux, vet_c, figura_vet_i = pcaClassificacao(img, imagensLabel, vetorImagens)
+
+        if len(img) != 0:
+            if usarPCA == True:
+                aux, vet_c, figura_vet_i = pcaClassificacao(img, imagensLabel, vetorImagens)
+            else:
+                aux, vet_c, figura_vet_i = forcaBrutaClassificacao(caminho, img, imagens_treino)
         else:
-            aux, vet_c, figura_vet_i = forcaBrutaClassificacao(caminho, img, imagens_treino)
+            vet_c = 0
+            aux = None
             
         if classificador(fn) == str(vet_c):
             
@@ -204,27 +258,41 @@ def reconhecedor(resultado, usarPCA,banco_imagens, imagens_teste, imagens_treino
             print('class = ' + str(vet_c))
             
             if usarPCA == True:
-                fig, axes = plt.subplots(1,2,sharex=True,sharey=True,figsize=(8,6))
-                if classificador(fn) == str(vet_c):
-                    fig.suptitle('PCA retorna: Igual')
+                if len(img) != 0:
+                    fig, axes = plt.subplots(1,2,sharex=True,sharey=True,figsize=(8,6))
+                    if classificador(fn) == str(vet_c):
+                        fig.suptitle('PCA retorna: Igual')
+                    else:
+                        fig.suptitle('PCA retorna: Diferente')
+                    axes[0].imshow(img.reshape(100,100), cmap="gray")
+                    axes[0].set_title("Imagem Teste")
+                    axes[1].imshow(figura_vet_i.reshape(100,100), cmap="gray")
+                    axes[1].set_title("Imagem Encontrada")
+                    plt.show()
                 else:
-                    fig.suptitle('PCA retorna: Diferente')
-                axes[0].imshow(img.reshape(100,100), cmap="gray")
-                axes[0].set_title("Imagem Teste")
-                axes[1].imshow(figura_vet_i.reshape(100,100), cmap="gray")
-                axes[1].set_title("Imagem Encontrada")
-                plt.show()
-            else: 
-                fig, axes = plt.subplots(1, 2,sharex=True,sharey=True,figsize=(8,6))
-                if classificador(fn) == str(vet_c):
-                    fig.suptitle('Forca bruta retorna: Igual')
+                    fig, axes = plt.subplots(1,2,sharex=True,sharey=True,figsize=(8,6))
+                    fig.suptitle('PCA retorna: Não encontrado')
+                    axes[0].imshow(imgNaoEncontrado.reshape(100,100), cmap="gray")
+                    axes[0].set_title("Imagem Teste")
+                    plt.show()
+            else:
+                if len(img) != 0: 
+                    fig, axes = plt.subplots(1, 2,sharex=True,sharey=True,figsize=(8,6))
+                    if classificador(fn) == str(vet_c):
+                        fig.suptitle('Forca bruta retorna: Igual')
+                    else:
+                        fig.suptitle('Forca bruta retorna: Diferente')
+                    axes[0].imshow(img.reshape(100,100), cmap="gray")
+                    axes[0].set_title('Imagem Teste')
+                    axes[1].imshow(figura_vet_i.reshape(100,100), cmap="gray")
+                    axes[1].set_title('Imagem Encontrada')
+                    plt.show()
                 else:
-                    fig.suptitle('Forca bruta retorna: Diferente')
-                axes[0].imshow(img.reshape(100,100), cmap="gray")
-                axes[0].set_title('Imagem Teste')
-                axes[1].imshow(figura_vet_i.reshape(100,100), cmap="gray")
-                axes[1].set_title('Imagem Encontrada')
-                plt.show()
+                    fig, axes = plt.subplots(1,2,sharex=True,sharey=True,figsize=(8,6))
+                    fig.suptitle('Forca Bruta: Não encontrado')
+                    axes[0].imshow(imgNaoEncontrado.reshape(100,100), cmap="gray")
+                    axes[0].set_title("Imagem Teste")
+                    plt.show()
     return (certo*100.)/(certo+errado)
 
 def reconhecimento(imgTeste, usarPCA, banco_imagens, tipo_Mapeamento, n_indices):
@@ -244,4 +312,4 @@ def reconhecimento(imgTeste, usarPCA, banco_imagens, tipo_Mapeamento, n_indices)
 
 
 # Imagens-Teste, Usar Pca, Exibir Imagens Comparacao, Caminho Imagens banco, Tipo modelo, Numero de imagens por pessoa
-# reconhecimento(["3-1.jpg", "2-6.jpg", "4-3.jpg", "2-4.jpg", "5-5.jpg"],True, 'recdev-master/extras/facebookfaces/', 'FB', 9)
+reconhecimento(["3-1.jpg", "2-6.jpg", "4-3.jpg", "2-4.jpg", "5-5.jpg"],True, 'recdev-master/extras/facebookfaces/', 'FB', 9)
